e8e56c82a44372623ef96062423b811218b0c6f4
Tim Beckett <timbeckett@tims-air.myfiosgateway.com>
2021-06-12 14:11:44 -0400

    First attempt at hooking up graphql.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Layout from "@/components/Layout";
 import Link from "next/link";
 
+// places data
+import { getAllPlacesPosts } from "../lib/api";
+import axios from "axios";
+
 import Showcase from "@/components/Showcase";
 
 export default function HomePage() {
