c7e405baa4924db61e4b9db06344a0026f07b959
Tim Beckett <timbeckett@Tims-Air.home>
2021-06-24 17:06:26 -0400

    added ability to grab data on news single page

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -1,10 +1,47 @@
+import { useRouter } from "next/router";
 import Layout from "@/components/Layout";
-import Link from "next/link";
 
-export default function NewsItem() {
+import { API_URL } from "@/config/index";
+
+//import styles from "@/styles/Place.module.css";
+
+export default function PlacePage({ newsItem }) {
+  const router = useRouter();
+  console.log(newsItem.title);
   return (
     <Layout>
-      <h1>A News Item</h1>
+      <div>
+        <h1 dangerouslySetInnerHTML={{ __html: newsItem.title.rendered }}></h1>
+        <article dangerouslySetInnerHTML={{ __html: newsItem.content.rendered }}></article>
+      </div>
     </Layout>
   );
+}
+
+// export async function getStaticPaths() {
+//   const res = await fetch(`${API_URL}places`);
+//   const places = await res.json();
+
+//   const paths = places.map(place => {
+//     params: {
+//       slug: place.slug;
+//     }
+//   });
+//   return {
+//     paths: [],
+//     fallback: false
+//   };
+// }
+
+export async function getServerSideProps({ query: { slug } }) {
+  // https://uraniumcity-history.com/wp-json/wp/v2/places?slug=%22sun-dogs-yellowcake-extract-chapter-8%22
+  const res = await fetch(`${API_URL}news?slug="${slug}"`);
+  console.log(res);
+  const news = await res.json();
+
+  return {
+    props: {
+      newsItem: news[0]
+    }
+  };
 }
